45f867c7380c183326c3fa2635b7ac42848d3d66
quetz05 <quetz05@gmail.com>
2014-06-04 00:32:26 +0200

    Dodanie klasy Row

---
+++ src/com/google/gwt/sample/stockwatcher/client/BaseRow.java
@@ -0,0 +1,16 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+public class BaseRow {
+	
+	public BaseRow(String name, String year, String category)
+	{
+		this.name = name;
+		this.year = year;
+		this.category = category;		
+		
+	}
+	
+	public String name;
+	public String category;
+	public String year;
+}
